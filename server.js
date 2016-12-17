307e2ef7c4948f7c19720fae1cddf94ca42d5db9
Dr. Iaroslav Zhbankov <yzhbankov@gmail.com>
2016-12-17 19:28:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -54,6 +54,6 @@ app.get('/:id', function (req, res) {
 
 });
 
-app.listen(8080, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('Ready. Listening port 8080');
 });
