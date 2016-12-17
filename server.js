a212582b2cc39d38718abe605296f979daed5fb3
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2016-12-17 16:26:56 +0200

    add inserting and search of urls from db

--- server.js
+++ server.js
@@ -3,6 +3,9 @@
  */
 var express = require('express');
 var app = express();
+var shortid = require('shortid');
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/url-shorter';
 
 app.use(express.static('public'));
 app.get('/', function (req, res) {
@@ -10,8 +13,33 @@ app.get('/', function (req, res) {
 });
 
 app.get('/new/:url?', function (req, res) {
+    var shorter_id = shortid.generate();
+    MongoClient.connect(url, function (err, db) {
+        console.log("Connected correctly to server");
 
-    res.send(req.params.url);
+        db.collection('shorturl').insertOne({"natural_url": req.params.url, "short_url": shorter_id}, function (err, result) {
+            if (!err) {
+                console.log("inserted successfuly");
+            }
+        });
+        db.close();
+    });
+    res.send({
+        "original_url": req.params.url,
+        "short_url": shorter_id
+    });
+});
+app.get('/get/:id', function (req, res) {
+    console.log(req.params.id);
+    MongoClient.connect(url, function (err, db) {
+        db.collection('shorturl').findOne({
+            "short_url": req.params.id
+        }, function (err, result) {
+            res.send(result);
+        });
+        db.close();
+    });
+
 });
 
 app.listen(8080, function () {
